ac05dc8534ddc6e9bee093d990405c1e0a45ad64
Chris <design@playbox.tech>
2023-09-05 20:57:50 -0500

    bulma

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Enigmatic Zone",
+	url: "https://enigmatic.zone/",
+	language: "en",
+	description: "Anthology Stories in Horror and Science Fiction",
+	author: {
+		name: "Chris A Moody",
+		email: "chris@chrisamoody.com",
+		url: "https://enigmatic.zone/about-me/"
+	}
+}
